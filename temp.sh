df9a8b63b611eed79dcc3d24c9324d9b86c3e51b
trevorColby <31737682+trevorColby@users.noreply.github.com>
2017-11-14 14:21:33 -0500

    More merge practice

--- temp.sh
+++ temp.sh
@@ -1,10 +1,16 @@
 Array=( Vim.md Tmux.md Developer.md Linux.md Math.md Art.md Github.md Canada.md WebDevelopment.md General.md Computer.md Fun.md Vocabulary.md Entertainment.md )
 for VARIABLE in "${Array[@]}"
 do
+	#A lot more complicated
 	#rm $VARIABLE
 	#touch $VARIABLE
 	#echo -e "$a*Trevor Colby*  \n*OptiRTC Intern*  \n*11/14/2017*  \n" >> $VARIABLE
 	echo "Currently empty bash script, does nothing at the moment"
 	#merge handled
+	#Lets make this more complicated
 done
 
+
+
+
+# Event a bit annoying
