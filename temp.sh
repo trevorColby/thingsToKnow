753a569c7d48e5e74727a00d951beb983604e12e
Trevor J Colby <trevor@trevor.localdomain.com>
2017-11-14 14:38:18 -0500

    Adding and removing temp

--- temp.sh
+++
@@ -1,9 +0,0 @@
-Array=( Vim.md Tmux.md Developer.md Linux.md Math.md Art.md Github.md Canada.md WebDevelopment.md General.md Computer.md Fun.md Vocabulary.md Entertainment.md )
-for VARIABLE in "${Array[@]}"
-do
-	#rm $VARIABLE
-	#touch $VARIABLE
-	#echo -e "$a*Trevor Colby*  \n*OptiRTC Intern*  \n*11/14/2017*  \n" >> $VARIABLE
-	echo "Currently empty bash script just using for practice and setup, does nothing at the moment"
-done
-
